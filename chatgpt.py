cb740ba2580e4397e09edcee9adbea5243723383
Bemi Folayan <89804473+bemifolayan@users.noreply.github.com>
2023-07-21 17:23:51 +0100

    Create chatgpt.py

---
+++ chatgpt.py
@@ -0,0 +1,31 @@
+import json
+import requests
+import os
+import openai
+openai.organization = "org-d5FqSHnir3fX9Upv0AHqEIAa"
+openai.api_key = os.environ.get('gpt_api_key')
+
+class ChatGPT:
+
+    def __init__(self, name):
+        self.name = name
+    
+    def careCalender(self):
+        plant_name = self.name
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {
+                    "role": "system",
+                    "content": "You will be provided with a plant name, your task is to generate a dictionary of length 30 with the days as keys of a real-life 30-days calender with insturctions on how to take care of the plant. Assume the plant is already grown"
+                },
+                {
+                    "role": "user",
+                    "content": "Plant Name: " + plant_name
+                }
+            ],
+            temperature=0.3,
+            max_tokens=2000
+        )
+        result = response["choices"][0]["message"]["content"]
+        return(result)
