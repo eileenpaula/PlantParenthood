108854c9a40240b6103c0a3394fbe886c1a5e937
eileenpaula <epaul037@fiu.edu>
2023-07-25 11:49:16 -0400

    working calendar

---
+++ chatgpt.py
@@ -0,0 +1,78 @@
+import json
+import requests
+from dotenv import load_dotenv
+load_dotenv()
+import os
+import openai
+openai.organization = "org-d5FqSHnir3fX9Upv0AHqEIAa"
+#openai.api_key1 = os.environ.get('gpt_api_key')
+openai.api_key= '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'
+
+class ChatGPT:
+
+    def __init__(self, name):
+        self.name = name
+
+    def careCalendar(self):
+        plant_name = self.name
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {
+                    "role": "system",
+                    "content": "You will be provided with a plant name, your task is to generate a dictionary with the integer number as keys of a real-life 7-days calender starting with today's date with insturctions on how to take care of the plant. Ensure these instructions can be done weekly. Assume the plant is already grown"
+                },
+                {
+                    "role": "user",
+                    "content": "Plant Name: " + plant_name
+                }
+            ],
+            temperature=0.3,
+            max_tokens=2000
+        )
+        result = response["choices"][0]["message"]["content"]
+        return(result)
+
+    def info(self):
+        plant_name = self.name
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {
+                    "role": "system",
+                    "content": "You will be provided with a plant name. Your task is to generate a dictionary with detailed information about the plant, including its description, preferred growing conditions (indoor/outdoor/either), plant type, climate, soil type, watering, and fun facts. Assume the plant is already grown."
+                },
+                {
+                    "role": "user",
+                    "content": "Plant Name: " + plant_name
+                }
+            ],
+            temperature=0.3,
+            max_tokens=2000
+        )
+        result = response["choices"][0]["message"]["content"]
+        return result
+
+    def is_plant(self):
+        plant_name = self.name
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {
+                    "role": "system",
+                    "content": "You will be provided with an object name. Your task is to return a true or false value on whether the object is a plant."
+                },
+                {
+                    "role": "user",
+                    "content": "object Name: " + plant_name
+                }
+            ],
+            temperature=0.3,
+            max_tokens=2000
+        )
+        result = response["choices"][0]["message"]["content"]
+        return result
+
+# test = ChatGPT('sunflower')
+# print(test.info())
+
