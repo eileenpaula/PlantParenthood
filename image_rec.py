fa06450c8ceb64d6b5e3e4497f0ccda6bf015213
David Excel Ozowara <122433522+Ozzy100@users.noreply.github.com>
2023-07-26 09:53:51 -0500

    Update image_rec.py

--- image_rec.py
+++ image_rec.py
@@ -21,13 +21,29 @@ class Image_Finder:
         data = result['result']['tags'][0]['tag']['en']
         return data
 
+    # def find_image_path(self):
+    #     image_path = os.path.join('static/files', 'image.jpg')
+    #     if os.path.exists(image_path):
+    #         return image_path
+
+    #     image_extensions = ['.jpg', '.png', '.jpeg']
+    #     for ext in image_extensions:
+    #         image_path = os.path.join('static/files', 'image' + ext)
+    #         if os.path.exists(image_path):
+    #             return image_path
+
+    #     return None
     def find_image_path(self):
-        image_extensions = ['.jpg', '.png','.jpeg']
-        for ext in image_extensions:
-            image_path = os.path.join('static/files', 'image' + ext)
-            if os.path.exists(image_path):
-                return image_path
-        return None
+        image_extensions = ['.jpg', '.png', '.jpeg']
+        image_files = [os.path.join('static/files', 'image' + ext) for ext in image_extensions]
+        image_files = [f for f in image_files if os.path.exists(f)]
+        if not image_files:
+            return None
+
+        # Sort the image files by their modification time (most recent first)
+        image_files.sort(key=lambda f: os.path.getmtime(f), reverse=True)
+
+        return image_files[0]
 
 
 # # Create an instance of the Image_Finder class
